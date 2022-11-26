c1b81afcb7024d6b9287124db95c576fc9a650dc
Alexander Ryzhov <a1exander@inbox.ru>
2022-11-26 19:04:30 +0300

    Add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,10 +1,18 @@
 """Role testing files using testinfra."""
 
+def test_user(host):
+    u = host.user("node-exporter")
+    assert u.exists
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_group(host):
+    g = host.group("node-exporter")
+    assert g.exists
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+def test_node_exporter_service(host):
+    s = host.service("node_exporter")
+    assert s.is_enabled
+    assert s.is_running
+
+def test_socket(host):
+    s = host.socket("tcp://0.0.0.0:9100")
+    assert s.is_listening
